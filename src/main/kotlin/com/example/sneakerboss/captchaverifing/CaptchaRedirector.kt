fd4ecd0a28bb89b01812fe1509a864e052816323
Szymon Pawlikowski <szymon.pawlikowski@precisely.com>
2023-01-23 22:13:29 +0100

    Captcha redirecting fix

--- src/main/kotlin/com/example/sneakerboss/captchaverifing/CaptchaRedirector.kt
+++ src/main/kotlin/com/example/sneakerboss/captchaverifing/CaptchaRedirector.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component
 class CaptchaRedirector {
 
     companion object {
-        private const val REDIRECT_URL = "https://stockx.com"
+        private const val REDIRECT_URL = "https://stockx.com/api/products/19ee5f88-55e0-4bdd-b855-1e9b3d2ba0b3?includes=market&currency=EUR&country=PL"
     }
 
     fun getCaptchaRedirectingUrl(): String {
